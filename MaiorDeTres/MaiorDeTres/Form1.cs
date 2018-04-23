2229c79a808245695e39654a510edfa63f79b0af
palomara <palomarangelll@hotmail.com>
2018-04-23 14:31:30 -0300

    bigger number

---
+++ MaiorDeTres/MaiorDeTres/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MaiorDeTres
+{
+    public partial class frmMaiorValorDeTres : Form
+    {
+        public frmMaiorValorDeTres()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            double ValorA = System.Convert.ToDouble(txbValorA.Text);
+            double ValorB = System.Convert.ToDouble(txbValorB.Text);
+            double ValorC = System.Convert.ToDouble(txbValorC.Text);
+
+            if (ValorA > ValorB && ValorA > ValorC)
+            {   
+                lblResposta.Text= "O maior valor é A";
+            }
+            else if (ValorB > ValorA && ValorB > ValorC)
+            {
+                lblResposta.Text = "O maior valor é B";
+            }
+            else if (ValorC > ValorA && ValorC > ValorB)
+            {
+                lblResposta.Text = "O maior valor é C";
+            }
+        }
+    }
+}
